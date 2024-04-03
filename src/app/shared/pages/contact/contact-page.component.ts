65f8f6c4115646823c8a1101d9a7ed87e0029653
Pavdev3 <pnunfe@gmail.com>
2024-04-03 18:48:50 +0200

    Update selector in contact-page.component.ts

--- src/app/shared/pages/contact/contact-page.component.ts
+++ src/app/shared/pages/contact/contact-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  selector: 'contact-page',
+  selector: 'shared-contact-page',
   templateUrl: './contact-page.component.html',
 })
 export class ContactPageComponent {}
