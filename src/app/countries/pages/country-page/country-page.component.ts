e1fbe8ff0fef41bc4d9840a953e1e7616aeada3c
Pavdev3 <pnunfe@gmail.com>
2024-04-15 20:05:53 +0200

    info country table redirect to info country

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -8,23 +8,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './country-page.component.html',
   styles: ``,
 })
-export class CountryPageComponent {
-  // public country?: Country;
-  // constructor(
-  //   private activatedRoute: ActivatedRoute,
-  //   private countriesService: CountriesService,
-  //   private router: Router
-  // ) {}
-  // ngOnInit(): void {
-  //   this.activatedRoute.params
-  //     .pipe(
-  //       filter(({ id }) => id !== undefined),
-  //       switchMap(({ id }) => this.countriesService.searchCountryByAlpha(id))
-  //     )
-  //     .subscribe((country) => {
-  //       if (!country) return this.router.navigateByUrl('');
-  //       return (this.country = country);
-  //     });
-  // }
-  // searchCountry(code: string) {}
-}
+export class CountryPageComponent {}
