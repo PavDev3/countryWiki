21a405deab6efb280ee57ece97071b4569d54ac5
Pavdev3 <pnunfe@gmail.com>
2024-04-16 10:45:42 +0200

    Refactor imports in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,13 @@ import { RouterOutlet } from '@angular/router';
 
 import { SearchBoxComponent } from './shared/components/searchBox/searchBox.component';
 import { SidebarComponent } from './shared/components/sideBar/sidebar.component';
-import { AboutPageComponent } from './shared/pages/about/about-page.component';
-import { HomePageComponent } from './shared/pages/home/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [
-    RouterOutlet,
-    HomePageComponent,
-    AboutPageComponent,
-    CommonModule,
-    SidebarComponent,
-    SearchBoxComponent,
-  ],
+  imports: [RouterOutlet, CommonModule, SidebarComponent, SearchBoxComponent],
 })
 export class AppComponent {
   title = 'countryWiki';
