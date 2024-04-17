d5d19989e010a5b54cab003501b46c2da5628924
Pavdev3 <pnunfe@gmail.com>
2024-04-17 09:21:27 +0200

    Add redirect to countries path in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'countries',
+  },
+  {
     path: 'countries',
     loadChildren: () =>
       import('./countries/services/countries.routes').then(
