8b22fe2febc79a81d9545f0b7d94f7f6a1b82157
Pavdev3 <pnunfe@gmail.com>
2024-04-17 08:51:51 +0200

    removed sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 
 import { CountryPageComponent } from './countries/pages/country-page/country-page.component';
-import { SidebarComponent } from './shared/components/sideBar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [SidebarComponent, CountryPageComponent],
+  imports: [CountryPageComponent],
 })
 export class AppComponent {
   title = 'countryWiki';
